bd04343995703f6a39ebdad227adfe67318007a1
Victor Lapp <Victor.Lapp@amersports.com>
2025-03-28 14:37:04 +0100

    update functions

--- main.py
+++ main.py
@@ -70,6 +70,5 @@ def main():
 
     return
 
-
 if __name__ == '__main__':
     main()
--- prompt.py
+++ prompt.py
@@ -1,7 +1,7 @@
 prompt = """
 J'ai envie de découvrir de nouvelles chansons. J'aime des chansons comme As it was d'Harry Styles 
 ou The Chain de Fleetwoods Mac. Trouve moi 30 morceaux (titre, artiste, album) pour ma playlist.
-Je veux des morceaux plutôt récents.
+Je veux des morceaux plutôt récents et d'artistes différents.
 Le résultat doit être UNIQUEMENT un fichier json avec le format:
 [
     {
